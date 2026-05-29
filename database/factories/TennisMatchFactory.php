e48090057feaa9f384deb52edb3a865c704dd5bb
Rameez Bhatti <xrkalix@gmail.com>
2026-05-29 20:55:27 +0500

    first one here

---
+++ database/factories/TennisMatchFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TennisMatch;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<TennisMatch>
+ */
+class TennisMatchFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
